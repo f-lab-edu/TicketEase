23c184f8890aa7507c4b08f88946056a0e6c8eb9
helloworld <smh0300@naver.com>
2023-08-31 20:36:30 +0900

    fix: springboot datasource error

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,1 @@
-create table if not exists ember (created_date datetime(6), id bigint not null auto_increment, nick_name varchar(255) not null, password varchar(255) not null, primary key (id)) engine=InnoDB
+create table if not exists member (created_date datetime(6), id bigint not null auto_increment, nick_name varchar(255) not null, password varchar(255) not null, primary key (id)) engine=InnoDB

6f5922b9588279a7b28577741d91794f7203f894
kcc <g2c1010@naver.com>
2023-09-03 19:55:19 +0900

    refactor: remove ticketService constructor

--- TE/src/main/java/com/TicketEase/TE/Ticket/TicketService.java
+++ TE/src/main/java/com/TicketEase/TE/Ticket/TicketService.java
@@ -9,10 +9,6 @@ import org.springframework.stereotype.Service;
 public class TicketService{
     private TicketRepository ticketRepository;
 
-    public TicketService(final TicketRepository ticketRepository) {
-        this.ticketRepository = ticketRepository;
-    }
-
     public GradeCount countTicketByGradeForPerformance(Long performanceId) {
         List<Ticket> tickets = ticketRepository.findByPerformanceId(performanceId);
         return GradeCount.from(tickets);

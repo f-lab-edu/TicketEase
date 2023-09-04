0ce0953644718b7aa615585a00ac83c8ebb2964c
helloworld <smh0300@naver.com>
2023-09-05 01:44:48 +0900

    fix: MainController 주석 해제

--- src/test/java/com/ticketease/te/main/MainControllerTest.java
+++ src/test/java/com/ticketease/te/main/MainControllerTest.java
@@ -1,32 +1,32 @@
-//package com.ticketease.te.main;
-//
-//import com.ticketease.te.config.Security;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-//import org.springframework.context.annotation.Import;
-//import org.springframework.test.web.servlet.MockMvc;
-//
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-//
-//@WebMvcTest(MainControllerTest.class)
-//@Import(Security.class)
-//class MainControllerTest {
-//
-//    @Autowired
-//    private MockMvc mvc;
-//
-//    @Test
-//    @DisplayName("메인페이지 확인")
-//    void giveNothing_whenRequestMainPage_thenReturnMainPage() throws Exception{
-//        //given
-//
-//        //when then
-//        mvc.perform(get("/"))
-//                .andExpect(status().isOk())
-//                .andExpect(view().name("index"));
-//    }
-//}
+package com.ticketease.te.main;
+
+import com.ticketease.te.config.Security;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(MainControllerTest.class)
+@Import(Security.class)
+class MainControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    @DisplayName("메인페이지 확인")
+    void giveNothing_whenRequestMainPage_thenReturnMainPage() throws Exception{
+        //given
+
+        //when then
+        mvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+}

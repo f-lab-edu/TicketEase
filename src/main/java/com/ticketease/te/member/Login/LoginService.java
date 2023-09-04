3c621d4d80329c2bee41660ab2abc77f65411da0
helloworld <smh0300@naver.com>
2023-09-04 19:06:29 +0900

    refactor: 패키지명 통일

---
+++ src/main/java/com/ticketease/te/member/Login/LoginService.java
@@ -0,0 +1,33 @@
+package com.ticketease.te.member.Login;
+
+import com.ticketease.te.member.Member;
+import com.ticketease.te.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class LoginService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Member> findMember = memberRepository.findByNickName(username);
+        if (findMember.isEmpty()){
+            throw new UsernameNotFoundException("계정정보를 확인해주세요");
+        }
+
+        return User.builder()
+                .username(findMember.get().getNickName())
+                .password(findMember.get().getPassword())
+                .roles("USER")
+                .build();
+    }
+}
---
+++ src/test/java/com/ticketease/te/member/signUp/SignUpServiceTest.java
@@ -0,0 +1,55 @@
+package com.ticketease.te.member.signUp;
+
+import com.ticketease.te.exception.SignUpExceptionHandler;
+import com.ticketease.te.member.Member;
+import com.ticketease.te.member.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+@DisplayName("회원가입 서비스")
+@ExtendWith(MockitoExtension.class)
+class SignUpServiceTest {
+
+    @InjectMocks
+    private SignUpService signUpService;
+
+    @Mock
+    private MemberRepository memberRepository;
+
+    @Test
+    @DisplayName("유효한 요청")
+    void givenSighUpDto_WhenNoDuplicateName_thenSignUpSuccess() {
+        //given
+        SignUpDto signUpDto = SignUpDto.of("hellWorld", "1234qwer", "1234qwer");
+
+        //when
+        when(memberRepository.findByNickName(signUpDto.nickName())).thenReturn(Optional.empty());
+
+        //then
+        assertDoesNotThrow(() -> signUpService.signUpUser(signUpDto));
+    }
+
+    @Test
+    @DisplayName("유효하지 않은 요청, 중복 닉네임 O")
+    void givenSighUpDto_WhenDuplicateNameExist_thenSignUpFail() {
+        //given
+        SignUpDto signUpDto = SignUpDto.of("hellWorld", "1234qwer", "1234qwer");
+
+        //when
+        when(memberRepository.findByNickName(signUpDto.nickName())).thenReturn(Optional.of(Member.of(signUpDto.nickName(), signUpDto.password())));
+
+        //then
+        assertThrows(SignUpExceptionHandler.class, () -> signUpService.signUpUser(signUpDto));
+    }
+
+}

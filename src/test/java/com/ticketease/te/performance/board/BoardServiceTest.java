f0ad43620eaf43d5636192d6b0ad28d3d874fec4
helloworld <smh0300@naver.com>
2023-09-05 21:51:01 +0900

    feat: 메인화면 공연게시판 및 검색기능

---
+++ src/test/java/com/ticketease/te/performance/board/BoardServiceTest.java
@@ -0,0 +1,57 @@
+package com.ticketease.te.performance.board;
+
+import com.ticketease.te.performance.PerformanceRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@DisplayName("게시판 서비스")
+@ExtendWith(MockitoExtension.class)
+class BoardServiceTest {
+
+    @InjectMocks
+    BoardService boardService;
+
+    @Mock
+    PerformanceRepository performanceRepository;
+
+    @Test
+    @DisplayName("검색어가 없을때 findAll 호출")
+    void givenNothing_whenFindPerformance_thenReturnAll(){
+        // Given
+        Pageable pageable = PageRequest.of(0, 10);
+
+        // When
+        when(performanceRepository.findAll(any(Pageable.class))).thenReturn(Page.empty());
+        Page<BoardDto> result = boardService.findPerformance(" ", pageable);
+
+        // Then
+        verify(performanceRepository).findAll(any(Pageable.class));
+        verify(performanceRepository, never()).findByNameContaining(anyString(), any(Pageable.class));
+    }
+
+    @Test
+    @DisplayName("검색어가 있을때 findByContaining 호출")
+    void givenSearch_whenFindPerformance_thenReturnSearchContaining(){
+        //given
+        Pageable pageable = PageRequest.of(0, 10);
+
+        // When
+        when(performanceRepository.findByNameContaining(anyString(), any(Pageable.class))).thenReturn(Page.empty());
+        Page<BoardDto> result = boardService.findPerformance("123123", pageable);
+
+        // Then
+        verify(performanceRepository, never()).findAll(any(Pageable.class));
+        verify(performanceRepository).findByNameContaining(anyString(), any(Pageable.class));
+    }
+
+}

13d2802c918bfabc6405e211388597e1e0b0b5bd
helloworld <smh0300@naver.com>
2023-09-11 22:57:49 +0900

    feat: merge conflict

--- src/main/java/com/ticketease/te/ticket/TicketService.java
+++ src/main/java/com/ticketease/te/ticket/TicketService.java
@@ -6,8 +6,6 @@ import com.ticketease.te.account.AccountRepository;
 import com.ticketease.te.account.AccountService;
 import com.ticketease.te.member.Member;
 import com.ticketease.te.member.MemberRepository;
-import com.ticketease.te.memberticket.MemberTicket;
-import com.ticketease.te.memberticket.MemberTicketRepository;
 import com.ticketease.te.memberticket.MemberTicketService;
 import com.ticketease.te.performance.Performance;
 import jakarta.transaction.Transactional;
@@ -26,26 +24,6 @@ public class TicketService{
     public GradeCount countTicketByGradeForPerformance(Performance performance) {
         return GradeCount.from(ticketRepository.findAllById(performance.getTicketIds()));
     }
-
-    @Transactional
-    public void purchaseTicket(final String nickName, final Long ticketId, final Integer requestSeatCount) {
-
-        Member member = memberRepository.findByNickName(nickName)
-                .orElseThrow(() -> new RuntimeException("존재하지 않는 이름입니다 닉네임 : " + nickName));
-        Ticket ticket = ticketRepository.findById(ticketId)
-                .orElseThrow(() -> new RuntimeException("존재하지 않는 상품입니다 상품Id : " + ticketId));
-        Account account = accountRepository.findById(member.getAccountId())
-                .orElseThrow(() -> new RuntimeException("존재하지 않는 계좌입니다"));
-        Seat seat  = ticket.getSeat();
-
-        accountService.deductAmount(account, ticket, requestSeatCount);
-
-        seat.reserveSeat(requestSeatCount);
-        ticketRepository.save(ticket);
-
-        memberTicketService.registerTicketForMember(member, ticket, requestSeatCount);
-    }
-}
 
     @Transactional
     public void purchaseTicket(final String nickName, final Long ticketId, final Integer requestSeatCount) {

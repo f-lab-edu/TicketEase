263a670980fa9951ddcf564251129abe7543f35b
kcc <g2c1010@naver.com>
2023-09-11 16:07:34 +0900

    refactor: create class Seat

--- src/main/java/com/ticketease/te/ticket/Ticket.java
+++ src/main/java/com/ticketease/te/ticket/Ticket.java
@@ -1,22 +1,27 @@
 package com.ticketease.te.ticket;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Ticket {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(updatable = false)
     private Long id;
-    private Integer seatCount;
+    @Embedded
+    private Seat seat;
     private Integer fixedPrice;
     @Enumerated(value = EnumType.STRING)
     private Grade grade;
@@ -26,22 +31,15 @@ public class Ticket {
     public void setPerformance(Long performanceId){
         this.performanceId = performanceId;
     }
-    protected Ticket(){}
 
-    private Ticket(final int seatCount, final int fixedPrice,
+    private Ticket(final Seat seat, final int fixedPrice,
                    final Grade grade) {
-        this.seatCount = seatCount;
+        this.seat = seat;
         this.fixedPrice = fixedPrice;
         this.grade = grade;
     }
-    public static Ticket of(final Integer seatCount, final Integer fixedPrice,
+    public static Ticket of(final Seat seat, final Integer fixedPrice,
                             final Grade grade){
-        return new Ticket(seatCount, fixedPrice, grade);
-    }
-    public void calculateSeat(final Integer requestCount){
-        if (this.seatCount < requestCount){
-            throw new RuntimeException("좌석이 부족합니다.");
-        }
-        this.seatCount -= requestCount;
+        return new Ticket(seat, fixedPrice, grade);
     }
 }

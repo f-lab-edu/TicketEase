491a298b1fc9a11ebb7e19ee4a4bb8c61c6cd2ca
kcc <g2c1010@naver.com>
2023-09-11 18:19:12 +0900

    refactor: create testCode

---
+++ src/test/java/com/ticketease/te/memberTicketService/MemberTicketServiceTest.java
@@ -0,0 +1,47 @@
+package com.ticketease.te.memberTicketService;
+
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import com.ticketease.te.member.Member;
+import com.ticketease.te.memberticket.MemberTicket;
+import com.ticketease.te.memberticket.MemberTicketRepository;
+import com.ticketease.te.memberticket.MemberTicketService;
+import com.ticketease.te.ticket.Grade;
+import com.ticketease.te.ticket.Seat;
+import com.ticketease.te.ticket.Ticket;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public class MemberTicketServiceTest {
+
+    @InjectMocks
+    private MemberTicketService memberTicketService;
+
+    @Mock
+    private MemberTicketRepository memberTicketRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    @DisplayName("멤버에게 티켓 등록 성공")
+    void registerTicketForMember_success() {
+        Member mockMember = Member.of("ko", "1234");
+        mockMember.addAccount(1L);
+        Ticket mockTicket = Ticket.of(Seat.of(100), 50_000, Grade.S);
+        Integer requestSeatCount = 1;
+
+        memberTicketService.registerTicketForMember(mockMember, mockTicket, requestSeatCount);
+
+        verify(memberTicketRepository, times(1)).save(any(MemberTicket.class));
+    }
+}
+

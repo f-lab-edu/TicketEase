a9bd8df9a4b91358c96e660925a92e84c625c4a6
kcc <g2c1010@naver.com>
2023-09-11 16:59:54 +0900

    refactor: TicketService AccountService deduct

---
+++ src/main/java/com/ticketease/te/account/AccountService.java
@@ -0,0 +1,20 @@
+package com.ticketease.te.account;
+
+import com.ticketease.te.ticket.Ticket;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AccountService {
+    private final AccountRepository accountRepository;
+
+    public void deductAmount(Account account, Ticket ticket, Integer requestSeatCount){
+        Integer totalPaymentAmount = ticket.getFixedPrice() * requestSeatCount;
+        if (account.getAmount() < totalPaymentAmount){
+            throw new RuntimeException("잔액이 부족합니다.");
+        }
+        Long remainingBalance = account.getAmount() - totalPaymentAmount;
+        accountRepository.save(Account.of(remainingBalance, account.getMemberId()));
+    }
+}

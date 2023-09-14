95c166d279e40edcf2b77e74c74a1cd63a19adb0
kcc <g2c1010@naver.com>
2023-09-14 17:33:38 +0900

    feat: ticketService entityFinderUtil

--- src/main/java/com/ticketease/te/ticket/TicketService.java
+++ src/main/java/com/ticketease/te/ticket/TicketService.java
@@ -2,14 +2,11 @@ package com.ticketease.te.ticket;
 
 
 import com.ticketease.te.account.Account;
-import com.ticketease.te.account.AccountRepository;
 import com.ticketease.te.account.AccountService;
 import com.ticketease.te.member.Member;
-import com.ticketease.te.member.MemberRepository;
-import com.ticketease.te.memberticket.MemberTicket;
-import com.ticketease.te.memberticket.MemberTicketRepository;
 import com.ticketease.te.memberticket.MemberTicketService;
 import com.ticketease.te.performance.Performance;
+import com.ticketease.te.util.EntityFinderUtil;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,22 +17,17 @@ public class TicketService{
     private final AccountService accountService;
     private final MemberTicketService memberTicketService;
     private final TicketRepository ticketRepository;
-    private final MemberRepository memberRepository;
-    private final AccountRepository accountRepository;
+    private final EntityFinderUtil entityFinderUtil;
 
     public GradeCount countTicketByGradeForPerformance(Performance performance) {
         return GradeCount.from(ticketRepository.findAllById(performance.getTicketIds()));
     }
-
     @Transactional
     public void purchaseTicket(final String nickName, final Long ticketId, final Integer requestSeatCount) {
 
-        Member member = memberRepository.findByNickName(nickName)
-                .orElseThrow(() -> new RuntimeException("존재하지 않는 이름입니다 닉네임 : " + nickName));
-        Ticket ticket = ticketRepository.findById(ticketId)
-                .orElseThrow(() -> new RuntimeException("존재하지 않는 상품입니다 상품Id : " + ticketId));
-        Account account = accountRepository.findById(member.getAccountId())
-                .orElseThrow(() -> new RuntimeException("존재하지 않는 계좌입니다"));
+        Member member = entityFinderUtil.findMemberByNickName(nickName);
+        Ticket ticket = entityFinderUtil.findTicketById(ticketId);
+        Account account = entityFinderUtil.findAccountByMember(member);
         Seat seat  = ticket.getSeat();
 
         accountService.deductAmount(account, ticket, requestSeatCount);
@@ -46,5 +38,16 @@ public class TicketService{
         memberTicketService.registerTicketForMember(member, ticket, requestSeatCount);
     }
 
+    @Transactional
+    public void cancelTicket(final String nickName, final Long ticketId,
+                             final Integer requestSeatCount,final Long memberTicketId){
+        Member member = entityFinderUtil.findMemberByNickName(nickName);
+        Ticket ticket = entityFinderUtil.findTicketById(ticketId);
+        Account account = entityFinderUtil.findAccountByMember(member);
+        Seat seat  = ticket.getSeat();
+        // 현재 날짜와 공연 날짜를 비교 하고 환불 할 수있는 지 확인 하는 로직 mt -> t -> p
 
+        // 데드락 발생할 수 있으므로 account ticket memberTicket 순으로 로직 처리
+
+    }
 }

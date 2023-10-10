3ab7a292d593c3d11e2fc46b7dff4872a00a05be
kcc <g2c1010@naver.com>
2023-10-10 18:44:33 +0900

    feat: 타임 아웃 제거

--- src/main/java/com/ticketease/te/ticket/TicketRepository.java
+++ src/main/java/com/ticketease/te/ticket/TicketRepository.java
@@ -6,17 +6,14 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 
 import jakarta.persistence.LockModeType;
-import jakarta.persistence.QueryHint;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
 	List<Ticket> findByPerformanceId(Long performanceId);
 
 	@Lock(LockModeType.PESSIMISTIC_READ)
-	@QueryHints({@QueryHint(name = "javax.persistence.lock.timeout", value = "5000")})
 	@Query("SELECT t FROM Ticket t WHERE t.id = :id")
 	Optional<Ticket> findByIdForUpdate(@Param("id") Long id);
 }
